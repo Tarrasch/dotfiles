a7c3db0a30d615c2a9302983772cc96c6efe8b29
Arash Rouhani <miffoljud@gmail.com>
2013-05-23 08:36:23 -0700

    Some decoupling from oh-my-zsh
     * Now ctrl+q is missing
     * Also still use oh-my-zsh themes

--- zsh/init-antigen.zsh
+++ zsh/init-antigen.zsh
@@ -1,18 +1,13 @@
 # Load antigen
 . ~/.zsh/antigen/antigen.zsh
 
-# Load oh-my-zsh
-antigen-lib
-
 # Auto env
 antigen-bundle kennethreitz/autoenv
 
 # My own plugins
 antigen-bundle Tarrasch/zsh-functional
 antigen-bundle Tarrasch/zsh-mcd
-
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen-bundle command-not-found
+antigen-bundle Tarrasch/zsh-command-not-found
 
 # Syntax highlighting bundle.
 antigen-bundle zsh-users/zsh-syntax-highlighting
