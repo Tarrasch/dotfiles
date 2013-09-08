ee0f22ebce4b50e08fb37656fcff4bd834b3cbe3
Arash Rouhani <miffoljud@gmail.com>
2013-09-08 19:00:05 +0200

    Add zsh-bd

--- zsh/init-antigen.zsh
+++ zsh/init-antigen.zsh
@@ -6,6 +6,7 @@ antigen-bundle Tarrasch/zsh-functional
 antigen-bundle Tarrasch/zsh-mcd
 antigen-bundle Tarrasch/zsh-command-not-found
 antigen-bundle Tarrasch/zsh-autoenv
+antigen-bundle Tarrasch/zsh-bd
 
 # Syntax highlighting bundle.
 antigen-bundle zsh-users/zsh-syntax-highlighting
