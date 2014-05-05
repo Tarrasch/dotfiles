284b3e2a29a79ea332f714546d7452a62cab394d
Arash Rouhani <arash@spotify.com>
2014-05-05 10:50:49 +0200

    Conditional history-substring-search loading
    I always had the issue that I needed different bindkey commands
    depending on the version of Ubuntu I ran and I don't want a dirty
    working directory!

--- zsh/init-antigen.zsh
+++ zsh/init-antigen.zsh
@@ -31,13 +31,22 @@ antigen-apply
 # bind UP and DOWN arrow keys for zsh-history-substring-search
 zmodload zsh/terminfo
 
-# The official hint below didn't work
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-#
-# But this below works ($terminfo[kcuu1]=='A') withou the '^[['
-# bindkey '^[[A' history-substring-search-up
-# bindkey '^[[B' history-substring-search-down
+# Hint from http://ubuntuforums.org/showthread.php?t=796627
+# Basically by sourcing the /etc/lsb-release you get some
+# shell variables set for you, like $DISTRIB_RELEASE
+[[ -s "/etc/lsb-release" ]] && . /etc/lsb-release
+
+if [[ $DISTRIB_RELEASE == "13.10" ]]
+then
+  bindkey "$terminfo[kcuu1]" history-substring-search-up
+  bindkey "$terminfo[kcud1]" history-substring-search-down
+elif [[ $DISTRIB_RELEASE == "12.04" ]]
+then
+  # But this below works ($terminfo[kcuu1]=='A') withou the '^[['
+  # At least for somewhat older Ubuntu versions like 12.04
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
+fi
 
 # And omg can somebody explain why the below doesn't work!?!?
 # echo $terminfo[kcuu1]
