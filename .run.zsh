a2f467b6a9e280dd6ba03ccfc909f380dbb354a2
Arash Rouhani <miffoljud@gmail.com>
2015-02-19 23:17:03 +0100

    Add .etc/sudoers.d/arash-extra-rules
    Basically so I can do sudo commands even on new terminals that I open
    without retyping my stupid password every time.

--- .run.zsh
+++ .run.zsh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 ##### Copy over files
 mkdir .local_backup/
-time=`date +%s`
+time=$(date +%s)
 bupf=.local_backup/$time/
 mkdir $bupf
 
@@ -29,10 +29,35 @@ git config --global core.excludesfile ~/.gitignore-global
 fc-cache -vf ~/.fonts
 
 ##### NeoBundle installation
-echo << EOL
+cat << EOL
 Don't forget to run:
 
+""""""""""""""""
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+""""""""""""""""
 
 EOL
+
+read \?" awaiting keypress ... "
+
+##### Configure sudoers file
+sudo echo "Just asking for password"
+echo "visudo --check *Before* copying arash-extra-rules ..."
+cat << EOL
+Ok, now will run
+
+sudo visudo --file=/etc/sudoers.d/arash-extra-rules
+
+and paste in the following:
+
+""""""""""""""""
+$(cat $PWD/.etc/sudoers.d/arash-extra-rules)
+""""""""""""""""
+
+EOL
+
+read \?" awaiting keypress ... "
+
+sudo visudo --file=/etc/sudoers.d/arash-extra-rules
+sudo visudo --check
