463be91ae1cbcd8f09f139a11e3bd9f185a7dca1
Arash Rouhani <miffoljud@gmail.com>
2017-02-11 10:54:31 +0700

    zsh: Simplify bindkey config for substring search

--- zsh/plugin-configs/history-substring-search.zsh
+++ zsh/plugin-configs/history-substring-search.zsh
@@ -1,24 +1,8 @@
 # bind UP and DOWN arrow keys for zsh-history-substring-search
 zmodload zsh/terminfo
 
-# Hint from http://ubuntuforums.org/showthread.php?t=796627
-# Basically by sourcing the /etc/lsb-release you get some
-# shell variables set for you, like $DISTRIB_RELEASE
-[[ -s "/etc/lsb-release" ]] && . /etc/lsb-release
-
-if [[ $DISTRIB_RELEASE == "13.10" ||
-      $DISTRIB_RELEASE == "14.04" ]]
-then
-  bindkey "$terminfo[kcuu1]" history-substring-search-up
-  bindkey "$terminfo[kcud1]" history-substring-search-down
-elif [[ $DISTRIB_RELEASE == "12.04" ]]
-then
-  # But this below works ($terminfo[kcuu1]=='A') withou the '^[['
-  # At least for somewhat older Ubuntu versions like 12.04
-  bindkey '^[[A' history-substring-search-up
-  bindkey '^[[B' history-substring-search-down
-else
-  # This worked for me on the debian machines at Spotify
-  bindkey '^[[A' history-substring-search-up
-  bindkey '^[[B' history-substring-search-down
-fi
+# It seems like the following works good for Ubuntu 13.10+,
+# I've now removed all intelligent version checking, because
+# at the time of writing all older versions are now history.
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
