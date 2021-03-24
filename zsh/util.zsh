cb57e529c5d55c9fb476d7bb2122ab4304ccd3a6
Arash Rouhani-Kalleh <rouhani@google.com>
2021-03-24 14:36:31 +0100

    Add new_or_attach tmux command

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -1,3 +1,19 @@
+new_or_attach () {
+  # "New or ATtach" - my tmux workflow
+  #
+  # Usage:
+  #
+  # nat work-on-taxes
+  tmux new -s $1 || tmux attach -t $1
+}
+
+# Autocompletion for existing sessions.
+_new_or_attach () {
+  reply=($(tmux list-sessions -F '#{session_name}' ))
+}
+
+compctl -V directories -K _new_or_attach new_or_attach 
+
 minify-video () {
   # I got extremely pissed by that it took hours to find a decent way to just
   # make a ****ing video take less disk space.  So that I will not forget this
